623bce899cb57f11688c94a89426346014f5a779
Phuffick <57110647+Phuffick@users.noreply.github.com>
2020-12-17 20:43:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,47 +3,29 @@
 
 
 int main() {
-
-	List list;
-
 	int key;
-
+	
+	List list;
 	list.AddToEnd(5);
-
 	list.AddToEnd(6);
-
 	list.AddToEnd(7);
-
 	list.AddToEnd(8);
-
 	list.AddToBegin(4);
-
 	list.AddToBegin(3);
-
 	list.AddToBegin(2);
-
 	list.AddToBegin(1);
-
 	list.ForwardPrint();
-
 	list.DeleteNodeEnd();
-
 	list.DeleteNodeBeg();
-
 	list.ForwardPrint();
 
 	std::cout << "Enter key: ";
-
 	std::cin >> key;
 
 	list.Find(key);
-
 	list.Remove(100, 4);
-
 	list.ForwardPrint();
-
 	list.Insert(200, 4);
-
 	list.ForwardPrint();
 
 	system("pause");

2e9856ad5b5b1477acc0c07801705a8e48923b62
Phuffick <57110647+Phuffick@users.noreply.github.com>
2020-12-17 20:50:03 +0300

    Update classes.h

--- classes.h
+++ classes.h
@@ -3,287 +3,196 @@
 
 
 class Node{
-
 	friend class List;
 
 private:
-
 	int data;
-
+	int key;
 	Node* last;
-
 	Node* next;
 
-	int key;
-
 public:
-
 	Node(int data, Node* last, Node* next) {
-	
 		this->data = data;
-
 		this->last = last;
-
 		this->next = next;
-	
 	};
 
 	Node(int data) {
-	
 		this->data = data;
-
 	};
-
 };
 
 
 class List{
-
 private:
-
 	Node* begin;
-
 	Node* end;
 
 public:
-
 	List() : begin(NULL), end(NULL) {};
-
+	
 	void AddToEnd(int data) {
-
 		Node* New = new Node(data, end, NULL);
-
 		end = New;
 
 		if (begin) {
-
 			Node* curr = begin;
-
+			
 			while (curr->next) {
-
 				curr = curr->next;
-
 			};
-
 			curr->next = New;
-
 		}
-
 		else{
-
 			begin = New;
-
 		}
 
 		Counter();
-	
 	};
 
 	void AddToBegin(int data) {
-	
 		Node* New = new Node(data, NULL, begin);
-
 		begin = New;
 
 		if (end) {
-
 			Node* curr = end;
 
 			while (curr->last) {
-
 				curr = curr->last;
-
 			}
 
 			curr->last = New;
-
 		}
-
 		else {
-
 			end = New;
-
 		}
 
 		Counter();
-	
 	};
 
 	void DeleteNodeEnd() {
-	
 		if (end) {
-
 			Node* curr = end->last;
-
 			delete curr->next;
-
 			curr->next = NULL;
-
 			end = curr;
-
+		}
+		else{
+			std::cout << "The list is empty. " << '\n';
 		}
 
-		else
-
-			std::cout << "The list is empty. " << std::endl;
-
 		Counter();
-	
 	};
 
 	void DeleteNodeBeg() {
-	
 		if (begin) {
-
 			Node* curr = begin->next;
-
 			delete curr->last;
-
 			begin = curr;
-
 		}
-
-		else
-
-			std::cout << "The list is empty. " << std::endl;
+		else{
+			std::cout << "The list is empty. " << '\n';
+		}
 
 		Counter();
 	
 	};
 
 	int Remove(int d, int key) {
-
 		Node* New = new Node(d);
-	
 		Node* curr = begin;
 
 		while (curr) {
-
-			if (curr->key == key) break;
-
+			if (curr->key == key){
+				break
+			}
 			curr = curr->next;
-
 		}
 
 		if (!curr) {
-
-			std::cout << "The is not key " << key << std::endl;
-
+			std::cout << "The is not key " << key << '\n';
 			return -1;
-
 		}
 
 		(curr->last)->next = New;
-
 		(curr->next)->last = New;
-
 		New->last = (curr->last);
-
 		New->next = (curr->next);
-
 		New->key = key;
-
 	};
 
 	int Insert(int d, int key) {
-	
 		Node* New = new Node(d);
-
 		Node* curr = begin;
 
 		while (curr) {
-
-			if (curr->key == key) break;
+			if (curr->key == key){
+				break;
+			}
 
 			curr = curr->next;
-
 		}
 
 		if (!curr) {
-
-			std::cout << "The is not key " << key << std::endl;
-
+			std::cout << "The is not key " << key << '\n';
+			
 			return -1;
-
 		}
 
 		(curr->last)->next = New;
-
 		curr->last = New;
-
 		New->last = (curr->last);
-
 		New->next = curr;
 
 		Counter();
-
 	};
 
 	void Find(int key) {
-
 		Node* curr = begin;
 	
 		while (curr) {
-
-			if (curr->key == key)
-
-				std::cout << key << " --- " << curr->data << std::endl;
+			if (curr->key == key){
+				std::cout << key << " --- " << curr->data << '\n';
+			}
 
 			curr = curr->next;
-
 		}
 
-		std::cout << std::endl;
-
+		std::cout << '\n';
 	};
 
 	void ForwardPrint() {
-
 		Node* curr = begin;
 	
-		while (curr)
-		{
-
-			std::cout << curr->key << " - " << curr->data << std::endl;
-
+		while (curr){
+			std::cout << curr->key << " - " << curr->data << '\n';
 			curr = curr->next;
-
 		}
 
-		std::cout << std::endl << std::endl;
+		std::cout << "\n\n";
 	
 	};
 
 	void BackPrint() {
-	
 		Node* curr = end;
 
 		while (curr) {
-
-			std::cout << curr->key << " - "<< curr->data << std::endl;
-
+			std::cout << curr->key << " - "<< curr->data << '\n';
 			curr = curr->last;
-
 		}
-
-		std::cout << std::endl << std::endl;
-	
+		
+		std::cout << "\n\n";
 	};
 
 	void Counter() {
-	
 		Node* curr = begin;
-
 		int i = 0;
 
 		while (curr) {
-
 			curr->key = ++i;
-
 			curr = curr->next;
-
 		}
-	
 	};
 
 };
